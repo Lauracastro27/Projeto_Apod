fddfeb927e3e1b4c93f9ca23d0c146fccff673a5
Laura Castro <Laura2702castro@gmail.com>
2022-02-06 21:18:50 -0300

    Arrumando os links e o estilo

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-const start = document.getElementById("start");
-
-start.addEventListener("click", function() {
-    window.location.href= "https://lauracastro27.github.io/Projeto_Apod/Apod.html"
-});
-const ini = document.getElementById("inicio");
-
-ini.addEventListener("click", function () {
-    window.location.href = "https://lauracastro27.github.io/Projeto_Apod/index.html"
-});
-
-const carr = document.querySelector("#carregar");
-
-carr.addEventListener("click", function () {
-    location.reload();
-});
-
 $(document).ready(function () {
     let today = new Date();
     let dia = today.getDate();
@@ -40,9 +23,8 @@ $('.video').addClass('none')
 botao.on('click', function () {
     $('html').css('height', 'auto')
     let data = $('#date').val()
-    const api = $.ajax({
-        url: `https://api.nasa.gov/planetary/apod?api_key=8wRUlMYfe2SO51TU7h1TWoa1O4rAcFDDiWyXx45g&date=${data} `,
-        success: function () {
+    const api = $.ajax({ url: `https://api.nasa.gov/planetary/apod?api_key=8wRUlMYfe2SO51TU7h1TWoa1O4rAcFDDiWyXx45g&date=${data} `,
+        success: function() {
             $('.descricao').text(api.responseJSON.explanation)
             $('.autor').text(api.responseJSON.copyright)
 
@@ -59,3 +41,28 @@ botao.on('click', function () {
         }
     })
 });
+
+const carr = document.querySelector("#carregar");
+
+carr.addEventListener("click", function () {
+    location.reload();
+});
+
+function inicio() {
+    window.location.href="https://lauracastro27.github.io/Projeto_Apod/index.html";
+}    
+
+function Apod() {
+    window.location.href="https://lauracastro27.github.io/Projeto_Apod/Apod.html";
+}    
+
+function instagram() {
+    window.location.href="https://www.instagram.com/laura._.castro27/"
+}
+
+function sobre() {
+    window.location.href="https://apod.nasa.gov/apod/lib/about_apod.html"
+}
+function nasa() {
+    window.location.href="https://www.nasa.gov/"
+}
